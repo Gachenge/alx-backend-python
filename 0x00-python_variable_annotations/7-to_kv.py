2e2784f24e2f1a494c1788b91904d3d00a1d228e
Gachenge <gachenge1@gmail.com>
2023-07-06 13:04:06 +0300

    python variable annotations

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,10 +1,10 @@
 #!/usr/bin/env python3
-from typing import Union, Tuple
 """
 a type-annotated function to_kv that takes a string k and an int OR float v
 as arguments and returns a tuple. The first element of the tuple is the
 string k
 """
+from typing import Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
--- 0x00-python_variable_annotations/9-element_length.py
+++ 0x00-python_variable_annotations/9-element_length.py
@@ -1,9 +1,9 @@
 #!/usr/bin/env python3
-from typing import Sequence, List, Tuple, Iterable
 """
 Annotate the below function’s parameters and return values with the
 appropriate types
 """
+from typing import Sequence, List, Tuple, Iterable
 
 
 def element_length(lst: Iterable[Sequence]) -> List[Tuple[Sequence, int]]:
