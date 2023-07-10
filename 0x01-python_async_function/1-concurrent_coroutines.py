1dbe0b8eada3b8c7e6748922d4a74b5b81eb3c6d
Gachenge <gachenge1@gmail.com>
2023-07-10 15:09:52 +0300

    clean up

--- 0x01-python_async_function/1-concurrent_coroutines.py
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -1,14 +1,21 @@
 #!/usr/bin/env python3
-"""concurrent execution"""
+
+"""
+Import wait_random from the previous python file that youve written
+write an async routine called wait_n that takes in 2 int arguments
+"""
+
 import asyncio
 from typing import List
-from heapq import nsmallest
+
 wait_random = __import__('0-basic_async_syntax').wait_random
 
 
 async def wait_n(n: int, max_delay: int) -> List[float]:
-    """concurrent execution"""
+    """spawn wait_random n times with the specified max_delay"""
     delays = []
-    for i in range(n):
-        delays.append(await wait_random(max_delay))
-    return nsmallest(n, delays)
+    tasks = [wait_random(max_delay) for _ in range(n)]
+    for task in asyncio.as_completed(tasks):
+        delay = await task
+        delays.append(delay)
+    return delays
